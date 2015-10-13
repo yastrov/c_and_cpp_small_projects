d98291511d8671594ed63606fea4516774a56478
Yuriy Astrov <yuriastrov@gmail.com>
2015-10-13 22:41:01 +0300

    Readen Books: more C++.

--- smallutilitys/readenbooks.cpp
+++ smallutilitys/readenbooks.cpp
@@ -1,6 +1,9 @@
 /*
 Program for read some binary file format with list, which contain list with readen books.
 C++ language.
+
+Compile: g++ -std=c++11 readenbooks.cpp -o rbooks
+
 Written by Yuri (Yuriy) Astrov*/
 #include <iostream>
 #include <iomanip>
@@ -10,8 +13,11 @@ Written by Yuri (Yuriy) Astrov*/
 #include <tchar.h>
 #include <algorithm>
 using namespace std;
-
+//#define CHECK_COMPARE_WITH_C_CAST
+//#define CHAR_MODE
 // We don't need to pack here< but I want to remember it.
+#ifdef CHAR_MODE
+const string mode_info = "CHAR MODE";
 //#pragma pack(push, r1, 1)
 struct fileinfo
 {
@@ -24,35 +30,81 @@ struct fileinfo
 };
 //#pragma pack(pop, r1)
 
-void readfrom(ifstream& stream, fileinfo *out)
+void readfrom(std::ifstream& stream, fileinfo *out)
 {
-	stream.read((char*)&out->number, sizeof(out->number));
-	stream.read((char*)&out->type, sizeof(out->type));
-	stream.read((char*)&out->sizeP, sizeof(out->sizeP));
-	stream.read((char*)&out->path, out->sizeP);
-	stream.read((char*)&out->sizeF, sizeof(out->sizeF));
-	stream.read((char*)&out->filename, out->sizeF);
+	//stream.read((char*)&out->number, sizeof(out->number));
+	stream.read(reinterpret_cast<char*>(&out->number), sizeof(out->number));
+	stream.read(reinterpret_cast<char*>(&out->type), sizeof(out->type));
+	stream.read(reinterpret_cast<char*>(&out->sizeP), sizeof(out->sizeP));
+	stream.read(reinterpret_cast<char*>(out->path), out->sizeP);
+	stream.read(reinterpret_cast<char*>(&out->sizeF), sizeof(out->sizeF));
+	stream.read(reinterpret_cast<char*>(out->filename), out->sizeF);
+#ifdef CHECK_COMPARE_WITH_C_CAST
+	cout << "---Check reinterpret_cast<char*>(out->filename) == reinterpret_cast<char*>(&out->filename): ";
+		//I check, it's valid.
+		if(reinterpret_cast<char*>(out->filename) == reinterpret_cast<char*>(&out->filename))
+			cout << "It's valid" << endl;
+		else
+			cout << "Warning int cast" << endl;
+#endif
 }
-
-void printelement(fileinfo item)
+#else
+// String Mode
+const string mode_info = "STRING MODE";
+struct fileinfo
 {
-	cout << endl;
-	cout << std::setw(11) << "Number:\t" << item.number << endl;
-	cout << std::setw(11) << "Type:\t" << item.type << endl;
-	cout << std::setw(11) << "File name:\t" << item.filename << endl;
-	cout << std::setw(11) << "Path:\t" << item.path << endl;
+	int number;
+	int type;
+	int sizeP;	// It for compability with printelement function.
+	string path;
+	int sizeF;	// It for compability with printelement function.
+	string filename;
+};
+
+void readfrom(std::ifstream& stream, fileinfo *out)
+{
+	stream.read(reinterpret_cast<char*>(&out->number), sizeof(out->number));
+	stream.read(reinterpret_cast<char*>(&out->type), sizeof(out->type));
+	stream.read(reinterpret_cast<char*>(&out->sizeP), sizeof(out->sizeP));
+	//Important! First - resize, second - get begin.
+	out->path.resize(out->sizeP, ' ');
+	char* begin = &*out->path.begin();
+	stream.read(begin, out->sizeP);
+	stream.read(reinterpret_cast<char*>(&out->sizeF), sizeof(out->sizeF));
+	out->filename.resize(out->sizeF, ' ');
+	begin = &*out->filename.begin();
+	stream.read(begin, out->sizeF);
+}
+#endif
+
+void printelement(std::ostream& stream, const fileinfo& item)
+{
+	stream << endl;
+	stream << std::setw(11) << "Number:\t" << item.number << endl;
+	stream << std::setw(11) << "Type:\t" << item.type << endl;
+	stream << std::setw(11) << "File name:\t" << item.filename << endl;
+	stream << std::setw(11) << "Path:\t" << item.path << endl;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale(LC_ALL, "RUSSIAN");
-	fileinfo item;
+	cout << "Compile struct and functions: " << mode_info << endl;
 	std::vector<fileinfo> items;
 	int count, i=0;
+#ifdef CHECK_COMPARE_WITH_C_CAST
+	cout << "---Check reinterpret_cast<char*>(&count) == (char*)&count: ";
+	//I check, it's valid.
+	if(reinterpret_cast<char*>(&count) == (char*)&count) {
+		cout << "It's valid" << endl;
+	} else {
+		cout << "Warning int cast" << endl;
+	}
+#endif
 	char ending[4];
-	std::ifstream input(_T(argv[0]), std::ifstream::binary);
+	std::ifstream input(_T(argv[1]), std::ifstream::binary);
 	if (input) {
-		input.read((char*)&count, sizeof(count));
+		input.read(reinterpret_cast<char*>(&count), sizeof(count));
 		cout << "Количество записей: " << count << endl;
 		cout << "Файлы:" << endl;
 		items.resize(count);
@@ -70,12 +122,17 @@ int _tmain(int argc, _TCHAR* argv[])
 			++i;
 		}
 		if (!input.eof())
-			input.read((char*)&ending, sizeof(ending));
+			input.read(reinterpret_cast<char*>(ending), sizeof(ending));
 		input.close();
-		cout << "Подробности: " << endl;
-		for_each(items.begin(), items.end(), printelement);
-		std::cout << endl;
 	}
+	/*
+	for_each(items.cbegin(), items.cend(), [](const fileinfo& item){
+		printelement(cout, item);
+	});
+	*/
+	for(const auto& item: items)
+		printelement(cout, item);
+	std::cout << endl;
 	return 0;
 }
 
